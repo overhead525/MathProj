76badaf40925500869ee6d96c5e30e8a8610bda4
overhead525 <ritchelcousar@outlook.com>
2021-03-08 18:37:07 -0500

    Added test for zero determinant

--- tests/matrices.test.ts
+++ tests/matrices.test.ts
@@ -7,6 +7,14 @@ test("getDeterminant method return correct determinant of inputted matrix", () =
     [5, 6],
     [7, -3],
   ];
+  const input2: number[][] = [
+    [4, 8],
+    [2, 4],
+  ];
+
   const result = MP.getDeterminant(input);
+  const result2 = MP.getDeterminant(input2);
+
   expect(result).toBe(-57);
+  expect(result2).toBe(0);
 });
