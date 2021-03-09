23f485371d1afa58f1958b34ed1074d9de72db99
overhead525 <ritchelcousar@outlook.com>
2021-03-09 13:32:33 -0500

    implemented new MatricesProcessor method for new feature

--- classes/MatricesProcessor.ts
+++ classes/MatricesProcessor.ts
@@ -1,5 +1,5 @@
 import { MathProcessor } from "./MathProcessor";
-import { Matrix, det, matrix } from "mathjs";
+import { Matrix, det, matrix, multiply, size } from "mathjs";
 
 export class MatricesProcessor extends MathProcessor {
   private parsedMatrix: Matrix;
@@ -7,5 +7,19 @@ export class MatricesProcessor extends MathProcessor {
   public getDeterminant(matrixExpresssion: number[][]): number {
     this.parsedMatrix = matrix(matrixExpresssion);
     return det(this.parsedMatrix);
+  }
+
+  public matrixMultiplication(matrix1: number[][], matrix2: number[][]) {
+    const size1 = size(matrix1);
+    const size2 = size(matrix2);
+
+    if (size1[1] !== size2[0]) {
+      throw new Error("Inner dimensions do not match");
+    }
+
+    const parsedMatrix1 = matrix(matrix1);
+    const parsedMatrix2 = matrix(matrix2);
+
+    return multiply(parsedMatrix1, parsedMatrix2);
   }
 }
